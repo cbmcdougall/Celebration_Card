77e4294d18f22360bd04b3a492b6e45c264f41b8
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-09-23 11:45:39 +0100

    Change file structure, add packages

---
+++ static/js/helpers.js
@@ -0,0 +1,47 @@
+function renderCard(greeting,event,message) {
+    const cardGreeting = document.getElementById("card-greeting");
+    const cardEvent = document.getElementById("card-event");
+    const cardMessage = document.getElementById("card-message");
+    let eventText;
+    switch (event) {
+        case "birthday":
+            eventText = "Happy birthday!";
+            break;
+        case "wedding":
+            eventText = "Congratulations on your wedding!";
+            break;
+        case "new job":
+            eventText = "Good luck in your new job!"
+            break;
+        default:
+            eventText = "Happy schmingling!"
+            break;
+    }
+    cardGreeting.textContent = greeting;
+    cardEvent.textContent = eventText;
+    cardMessage.textContent = message;
+}
+
+function deleteCard() {
+    const cardGreeting = document.getElementById("card-greeting");
+    const cardEvent = document.getElementById("card-event");
+    const cardMessage = document.getElementById("card-message");
+    cardGreeting.textContent = "";
+    cardEvent.textContent = "";
+    cardMessage.textContent = "";
+}
+
+function clearForm() {
+    const greetingForm = document.getElementById("greeting")
+    const eventForm = document.getElementsByName("eventType")
+    const messageForm = document.getElementById("message")
+    greetingForm.value = "";
+    eventForm.forEach(event => { event.checked = false });
+    messageForm.value = "";   
+}
+
+module.exports = {
+    renderCard,
+    deleteCard,
+    clearForm
+}
